148a14f9ce2f37f8f5156c11925eca3482cdc394
locnguyen <iot.loc.nguyen@gmail.com>
2020-09-18 07:24:59 +0700

    Update Frondend

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,25 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import {Layout, Button} from 'antd';
 import './App.css';
-
 const {Header}=Layout;
  
+
 function App() {
   return (
     <div className="container">
       <Layout>
         <Header className='header'>
           <h1>Wellcome Ant!</h1>
+          <button type="button" class="btn btn-primary" >Primary</button>
         </Header>
       </Layout>
       <div className="jumbotron">
         <h1>Wellcome Bootstrap
-          <p>Chuc ban may man lan sau ...</p>
+          <p>Chuc ban may man lan sau ...........1 </p>
         </h1>
         <button>Click here</button>
       </div>
     </div>
   );
 }
+
 
 export default App;
