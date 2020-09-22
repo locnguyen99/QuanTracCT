a3bedef3831bc477f8440036bff749b809c8b705
locnguyen <phuminh@maysoft.vn>
2020-09-22 07:53:41 +0700

    Update react

--- Frontend/ExampleFirst/my-app/src/App.tsx
+++ Frontend/ExampleFirst/my-app/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FilterableProductTable from './Components/FilterableProductTable';
+
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
         >
           Learn React
         </a>
+        <FilterableProductTable></FilterableProductTable>
       </header>
     </div>
   );
---
+++ Frontend/ExampleFirst/my-app/src/Components/FilterableProductTable.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './../CSS/FilterableProductTable.css';
+
+
+export default class FilterableProductTable extends React.Component {
+    render(){
+        console.log('funcion render');
+        return <div className='do'>Hello</div>;
+    }
+    componentDidMount(){
+        console.log('funcion didmount');
+        
+    }
+
+
+}
+
