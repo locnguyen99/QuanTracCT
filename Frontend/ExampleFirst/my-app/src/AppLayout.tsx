d9479ce6154f8595d4ffeb2f5aa5499fd5f9a008
locnguyen <phuminh@maysoft.vn>
2020-11-30 00:24:38 +0700

    update Sider

--- Frontend/ExampleFirst/my-app/src/AppLayout.tsx
+++ Frontend/ExampleFirst/my-app/src/AppLayout.tsx
@@ -25,52 +25,62 @@ export default class AppLayout extends React.Component {
     render() {
         return (
             <div>
-                <div className='hea'>
-                    <header>tertettttttttttttt</header>
-                </div>
-                <Layout>
-                    <div className='vuong'>
-                        <div className='buttop'>
-                            <div>
-                                <Button ></Button>
-                            </div>
-                            <div className='PG'>
-                                <b><i>PG</i></b>
-                            </div>
-                            <div className='SU'>
-                                <b><i>Survey</i></b>
-                            </div>
+                <div className='sider'>
+                    <div className='hea'>
+                        <div className='butPG'>
+                            <Button ></Button>
                         </div>
-                        <div className='tao'>
-                            <div>
-                                <Button ><AddCircleIcon /></Button>
-                            </div>
-                            <div className='create'>
-                                Tạo mới
-                            </div>
+                        <div className='PG'>
+                            <b><i>PG</i></b>
+                        </div>
+                        <div className='SU'>
+                            <b><i>Survey</i></b>
                         </div>
                     </div>
-                    <AppMenu />
-
-                    {/* <div>
+                    <div className='added'>
                         <div>
-                            <Layout>
-                                <Content>
-                                    <Breadcrumb>
-                                        <Breadcrumb.Item>User</Breadcrumb.Item>
-                                        <Breadcrumb.Item>Bill</Breadcrumb.Item>
-                                    </Breadcrumb>
-                                </Content>
-                            </Layout>
+                            <Button ><AddCircleIcon /></Button>
                         </div>
-                        <div className='ND'>
-                            {this.props.children}
+                        <div className='create'>
+                            Tạo mới
                         </div>
-                    </div> */}
-                </Layout>
-                <div className='foo'>
-                    <footer>wjfkwjfkjwfjwfw</footer>
+                    </div>
+                    <div>
+                        <AppMenu />
+                    </div>
+                    <div className='foo'>
+                        <footer>wjfkwjfkjwfjwfw</footer>
+                    </div>
                 </div>
+                {/* <div>
+                    <div>
+                        <h1>header</h1>
+                    </div>
+                    <div>
+                        <Layout>
+                            <div className='vuong'>
+                            </div>
+                            <div>
+                                <div>
+                                    <Layout>
+                                        <Content>
+                                            <Breadcrumb>
+                                                <Breadcrumb.Item>User</Breadcrumb.Item>
+                                                <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                                            </Breadcrumb>
+                                        </Content>
+                                    </Layout>
+                                </div>
+                                <div className='ND'>
+                                    {this.props.children}
+                                </div>
+                            </div>
+                        </Layout>
+                    </div>
+                    <div>
+                        <h2>footer</h2>
+                    </div>
+                </div> */}
             </div>
         );
     }
