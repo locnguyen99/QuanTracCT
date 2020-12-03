5cbe1b0a194aedaae8b00d421a1eb2f536cf44c0
locnguyen <phuminh@maysoft.vn>
2020-12-03 17:08:42 +0700

    12/3

--- Frontend/ExampleFirst/my-app/src/LoginLayout.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Layout } from 'antd';
-import './Login.scss'
-import './Login.scss'
-
-
-
--- Frontend/ExampleFirst/my-app/src/Signin.tsx
+++ Frontend/ExampleFirst/my-app/src/Signin.tsx
@@ -139,3 +139,4 @@ export default function SignIn() {
         </div >
     );
 }
+//yy
